616dfd1374def661c54c3b3e0b62445219ddec17
zhaoliwei <zhaoliwei02@meituan.com>
2018-09-24 12:42:43 +0800

    跳跃表实现细节

--- server/dataStructure/skiplist/SkipListNode.cpp
+++ server/dataStructure/skiplist/SkipListNode.cpp
@@ -5,7 +5,7 @@
 #include "SkipListNode.h"
 
 SkipListNode::SkipListNode(const SkipListType& type, void* obj, double score, unsigned int level) : type(type) {
-    this->obj = type.dup(obj);
+    this->obj = obj;
     this->score = score;
     for (int i = 0; i < level; i++) {
         this->levels.push_back(SkipListLevel());
