900ad8f9908138826be5341344493cccd03aa523
zhaoliwei <zhaoliwei02@meituan.com>
2018-10-10 23:17:04 +0800

    添加部分压缩列表的实现

--- server/dataStructure/ziplist/ZipList.cpp
+++ server/dataStructure/ziplist/ZipList.cpp
@@ -14,3 +14,95 @@ ZipList::ZipList() {
 int ZipList::push(unsigned char* s, uint32_t len, int where) {
 
 }
+
+unsigned char* ZipList::getEntry(uint32_t index) {
+
+}
+
+unsigned char* ZipList::findEntry(unsigned char* entry, uint32_t len, uint16_t skip) {
+
+}
+
+unsigned char* ZipList::nextEntry(unsigned char* entry) {
+    // s为空或者s指向尾结点
+    if (NULL == entry || ZIP_END == this->entries) {
+        return NULL;
+    }
+
+    return entry + decodeEntryLen(entry);
+}
+
+unsigned char* ZipList::prevEntry(unsigned char* entry) {
+    // entry为空或者指向头结点
+    if (NULL == entry || entry == this->entries) {
+        return NULL;
+    }
+
+    // entry指向尾节点
+    if (ZIP_END == *entry) {
+        return ZIP_END == this->zltail ? NULL : this->zltail;
+    }
+
+    return entry - decodePrevEntryLength(entry);
+}
+
+int ZipList::merge(ZipList zl) {
+
+}
+
+uint16_t ZipList::length() {
+    return this->len;
+}
+
+uint32_t ZipList::blobLen() {
+    return this->bytes;
+}
+
+int ZipList::deleteEntry(unsigned char* entry) {
+
+}
+
+uint32_t ZipList::decodePrevEntryLength(unsigned char* entry) {
+    uint8_t size = decodePrevLenSize(entry);
+    if (ZIP_PREVENTRY_SHORT == size) {
+        return *entry;
+    } else {
+        return *(reinterpret_cast<uint32_t*>(entry + 1));
+    }
+}
+
+uint8_t ZipList::decodePrevLenSize(unsigned char* entry) {
+    if (NULL == entry) {
+        return -1;
+    }
+
+    return ZIP_PREVENTRY_LONG_FLAG == *entry ? ZIP_PREVENTRY_LONG : ZIP_PREVENTRY_SHORT;
+}
+
+uint32_t ZipList::decodeEntryLen(unsigned char* entry, uint8_t prevLenSize) {
+    char* encoding = entry + prevLenSize;
+    uint8_t flag = *encoding & 0xc0;
+
+    uint8_t encodingSize = 0;
+    uint32_t contentSize = 0;
+    switch (flag) {
+        case 0x00:
+            encodingSize = 1;
+            contentSize = *encoding & 0x3F;
+            break;
+        case 0x40:
+            encodingSize = 2;
+            contentSize = *reinterpret_cast<uint16_t*>(encoding) & 0x3FFF;
+            break;
+        case 0x80:
+            encodingSize = 5;
+            contentSize = *reinterpret_cast<uint32_t*>(encoding + 1) & 0xFFFFFFFF;
+            break;
+        case 0xc0:
+            encodingSize = 1;
+            contentSize = 1;
+            break;
+    }
+
+    return prevLenSize + encodingSize + contentSize;
+}
--- server/dataStructure/ziplist/ZipList.h
+++ server/dataStructure/ziplist/ZipList.h
@@ -5,20 +5,28 @@
 #ifndef FLYDB_ZIPLIST_H
 #define FLYDB_ZIPLIST_H
 
+const uint8_t ZIP_END = 255;
+const uint8_t ZIP_PREVENTRY_LONG_FLAG = 0XFE;
+const uint8_t ZIP_PREVENTRY_LONG = 5;
+const uint8_t ZIP_PREVENTRY_SHORT = 1;
+
 class ZipList {
 public:
     ZipList();
     int push(unsigned char* s, uint32_t len, int where);
     unsigned char* getEntry(uint32_t index);
-    unsigned char* findEntry(unsigned char* s, uint32_t len, uint16_t skip);
-    unsigned char* nextEntry(unsigned char* s);
-    unsigned char* prevEntry(unsigned char* s);
+    unsigned char* findEntry(unsigned char* entry, uint32_t len, uint16_t skip);
+    unsigned char* nextEntry(unsigned char* entry);
+    unsigned char* prevEntry(unsigned char* entry);
     int merge(ZipList zl);
     uint16_t length();
     uint32_t blobLen();
-    int deleteEntry(unsigned char* s);
+    int deleteEntry(unsigned char* entry);
 
 private:
+    uint32_t decodePrevEntryLength(unsigned char* entry);
+    uint8_t decodePrevLenSize(unsigned char* entry);
+    uint32_t decodeEntryLen(unsigned char* entry, uint8_t prevLenSize);
     uint32_t bytes;
     uint32_t zltail;
     uint16_t len;
