733a1e48cc0d52ee4f8001a426505b1fae1aacea
zhaoliwei <zhaoliwei02@meituan.com>
2018-10-13 21:32:18 +0800

    完善DBDictType

--- server/db/DBDictType.cpp
+++ server/db/DBDictType.cpp
@@ -4,8 +4,13 @@
 
 #include "DBDictType.h"
 #include "../dataStructure/dict/Dict.h"
+#include "../dataStructure/flyObj/FlyObj.h"
 
 uint64_t DBDictType::hashFunction(const void *key) const {
+    if (NULL == key) {
+        return 0;
+    }
+
     return dictStrHash(key);
 }
 
@@ -18,13 +23,32 @@ void* DBDictType::valDup(void *obj) const {
 }
 
 int DBDictType::keyCompare(const void *key1, const void *key2) const {
+    if (NULL == key1 && NULL == key2) {
+        return 0;
+    }
+    if (NULL == key1) {
+        return -1;
+    }
+    if (NULL == key2) {
+        return 1;
+    }
+
     return dictStrKeyCompare(key1, key2);
 }
 
 void DBDictType::keyDestructor(void *key) const {
+    if (NULL == key) {
+        return;
+    }
+
     return dictStrDestructor(key);
 }
 
 void DBDictType::valDestructor(void *obj) const {
-    // todo(zlw) FlyObj;
+    if (NULL == obj) {
+        return;
+    }
+
+    FlyObj* flyObj = reinterpret_cast<FlyObj *>(obj);
+    flyObj->decrRefCount();
 }
